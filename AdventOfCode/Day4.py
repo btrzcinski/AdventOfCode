edc78c309a8377cfa761283b8a1c660fe5c2cbee
Barnett Trzcinski <barnett@microsoft.com>
2015-12-04 17:54:56 -0800

    Small changes to save some time

--- AdventOfCode/Day4.py
+++ AdventOfCode/Day4.py
@@ -4,11 +4,12 @@ def lowest_salt_with_leading_zeroes(secret_key, num_zeroes=5):
     leading_zeroes = "0" * num_zeroes
     salt = 0
     md5_hash = "abcdef"
+    encoded_secret_key = secret_key.encode('utf-8')
     while not md5_hash.startswith(leading_zeroes):
         salt += 1
         m = hashlib.md5()
-        m.update(secret_key.encode('utf-8'))
-        m.update(("%d" % (salt,)).encode('utf-8'))
+        m.update(encoded_secret_key)
+        m.update(str(salt).encode('utf-8'))
         md5_hash = m.hexdigest()
     return salt
 
