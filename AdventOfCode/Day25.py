053c3d6f117be7c603181a7b5e7e0dfaeb20a297
Barnett Trzcinski <btrzcinski@gmail.com>
2015-12-26 03:04:46 -0500

    Switched to modular exponentation which is much faster

--- AdventOfCode/Day25.py
+++ AdventOfCode/Day25.py
@@ -3,12 +3,20 @@ def n_from_row_col(row, col):
     n += sum(range(row+1, col+row))
     return n
 
+# Adapted from https://en.wikipedia.org/wiki/Modular_exponentiation
+def modexp(base, exp, mod):
+    if mod == 1: return 0
+    result = 1
+    base %= mod
+    while exp > 0:
+        if exp % 2 == 1:
+            result = (result * base) % mod
+        exp >>= 1
+        base = (base * base) % mod
+    return result
+
 def code_for_n(n):
-    code = 20151125 # start
-    for _ in range(1, n):
-        code *= 252533
-        code %= 33554393
-    return code
+    return (20151125 * modexp(252533, n - 1, 33554393)) % 33554393
 
 def code_for_row_col(row, col):
     return code_for_n(n_from_row_col(row, col))
