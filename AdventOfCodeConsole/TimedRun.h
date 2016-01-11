ea03420aff1aef15eea660c02954fe82c0031f50
Barnett Trzcinski <btrzcinski@gmail.com>
2016-01-10 22:37:19 -0800

    Made TimedRun.h cross-platform

--- AdventOfCodeConsole/TimedRun.h
+++ AdventOfCodeConsole/TimedRun.h
@@ -1,18 +1,20 @@
-#pragma once
+#ifndef __TIMED_RUN_H
+#define __TIMED_RUN_H
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#include <Windows.h>
-
+#include <chrono>
 #include <iostream>
 #include <iomanip>
 #include <functional>
 
 void print_time_for_run(std::function<void(void)> f)
 {
-    auto start = GetTickCount64();
+    using std::chrono::steady_clock;
+    auto start = steady_clock::now();
     f();
-    auto t = GetTickCount64() - start;
-    std::cout << "(" << (t / 1000) << "." << std::setw(3) << (t % 1000) << "s)" << std::endl;
+    auto end = steady_clock::now();
+    auto t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "(" << (t / 1000) << "." << std::setfill('0') << std::setw(3) << (t % 1000) << "s)" << std::endl;
 }
+
+#endif // __TIMED_RUN_H
+
